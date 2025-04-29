97ff621a625be15e1acfe42098f0b26a8b7bde9c
ltsenior25 <bepop@cps.edu>
2025-04-29 10:02:47 -0500

    8py

--- a8.py
+++ a8.py
@@ -2,6 +2,26 @@ from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+print("\n\nTraining XOR\n\n")
+xor_training_data = [
+    ([1, 1], [0]), 
+    ([1, 0], [1]), 
+    ([0, 1], [1]), 
+    ([0, 0], [0])
+    
+    ]
 
+xorn = NeuralNet(2, 1, 1)
+xorn.train(xor_training_data,iters=10000)
+print(xorn.test_with_expected(xor_training_data))
 
-
+print("\n\nTraining XOR\n\n")
+xor_training_data = [
+    ([0.9,0.6,0.8,0.3,0.1],[1])
+    ([0.8,0.8,0.4,0.6,0.4],[1])
+    ([0.7,0.2,0.4,0.6,0.3],[1])
+    ([0.5,0.5,0.8,0.4,0.8],[0])
+]
+xorn = NeuralNet(2,1,1)
+xorn.train(xor_training_data,iters=10000)
+print(xorn.test_with_expected(xor_training_data))
